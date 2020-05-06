c21966a0b44807c8a6ccd5a0a7754f6f25818dce
Mark <mark.hall@work.room3b.eu>
2020-05-06 20:55:56 +0100

    Added small screen test

--- tests/basic/landing.ts
+++ tests/basic/landing.ts
@@ -4,8 +4,14 @@ import { request } from 'http';
 fixture(`Landing Page`).beforeEach(async (test) => {
     const req = request('http://localhost:6543/tests/create');
     req.end();
+    await test.resizeWindow(1024, 768);
 }).page('http://localhost:6543/');
 
 test('Has the app title', async (test) => {
     await test.expect(Selector('.top-bar-left .menu .menu-text').innerText).eql('Experiment Support System');
+});
+
+test('Check warning on small screens', async (test) => {
+    await test.resizeWindow(800, 600);
+    await test.expect(Selector('h1', { visibilityCheck: true }).innerText).eql('Display unsupported');
 });
