def0f8563132bb443a316901be9790bbebd475c8
Mark <mark.hall@work.room3b.eu>
2020-05-13 20:59:07 +0100

    Added login tests

--- src/ess/tests/views.py
+++ src/ess/tests/views.py
@@ -1,5 +1,8 @@
+from hashlib import sha512
 from pyramid.view import view_config
+from secrets import token_hex
 
+from ess.models import User
 from ess.models.meta import Base
 
 
@@ -8,10 +11,23 @@ def init_db(request):
     Base.metadata.create_all(request.dbsession.bind)
 
 
+def create_user_1(request):
+    user = User(email='test1@example.com')
+    user.salt = token_hex(32)
+    hash = sha512()
+    hash.update(user.salt.encode('utf-8'))
+    hash.update(b'$$test1')
+    user.password = hash.hexdigest()
+    user.status = 'active'
+    request.dbsession.add(user)
+    return user
+
+
 def create(request):
     """Handles the setup of test 1"""
     init_db(request)
-    if 'object' in request.params:
-        for obj in request.params.get_all('object'):
-            pass
+    if 'obj' in request.params:
+        for obj in request.params.getall('obj'):
+            if obj == 'user1':
+                create_user_1(request)
     return {}
---
+++ tests/user/login.ts
@@ -0,0 +1,37 @@
+import { Selector } from 'testcafe';
+import { request } from 'http';
+
+fixture(`Registration`).beforeEach(async (test) => {
+    const req = request('http://localhost:6543/tests/create?obj=user1');
+    req.end();
+    await test.resizeWindow(1100, 800);
+}).page('http://localhost:6543/');
+
+test('Successful login', async (test) => {
+    await test.click(Selector('a').withText('Sign in'));
+    await test.expect(Selector('h1').innerText).eql('Sign in');
+    await test.typeText(Selector('input[name="email"]'), 'test1@example.com');
+    await test.typeText(Selector('input[name="password"]'), 'test1');
+    await test.click(Selector('button').withText('Sign in'));
+    await test.expect(Selector('a').withText('Sign out').innerText).eql('Sign out')
+});
+
+test('Failed login 1', async (test) => {
+    await test.click(Selector('a').withText('Sign in'));
+    await test.expect(Selector('h1').innerText).eql('Sign in');
+    await test.typeText(Selector('input[name="email"]'), 'test2@example.com');
+    await test.typeText(Selector('input[name="password"]'), 'test2');
+    await test.click(Selector('button').withText('Sign in'));
+    await test.expect(Selector('input[name="email"] + span.form-error').innerText).eql('Either there is no user with this e-mail address or the password is incorrect.');
+    await test.expect(Selector('input[name="password"] + span.form-error').innerText).eql('Either there is no user with this e-mail address or the password is incorrect.');
+});
+
+test('Failed login 2', async (test) => {
+    await test.click(Selector('a').withText('Sign in'));
+    await test.expect(Selector('h1').innerText).eql('Sign in');
+    await test.typeText(Selector('input[name="email"]'), 'test1@example.com');
+    await test.typeText(Selector('input[name="password"]'), 'test2');
+    await test.click(Selector('button').withText('Sign in'));
+    await test.expect(Selector('input[name="email"] + span.form-error').innerText).eql('Either there is no user with this e-mail address or the password is incorrect.');
+    await test.expect(Selector('input[name="password"] + span.form-error').innerText).eql('Either there is no user with this e-mail address or the password is incorrect.');
+});
