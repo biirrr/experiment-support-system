3b197644bcc1479b64cc7aa65f38f76d7e6638de
Mark Hall <mark.hall@work.room3b.eu>
2020-06-01 12:59:36 +0100

    Added missing aria-labels

--- src/js/apps/experiment-editor/src/views/Results.vue
+++ src/js/apps/experiment-editor/src/views/Results.vue
@@ -3,14 +3,14 @@
         <nav>
             <ul class="menu align-right">
                 <li role="presentation">
-                    <a role="menuitem" @click="reload">
+                    <a role="menuitem" aria-label="Reload results" @click="reload">
                         <svg viewBox="0 0 24 24" class="icon mdi">
                             <path d="M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z" />
                         </svg>
                     </a>
                 </li>
                 <li role="presentation">
-                    <a role="menuitem">
+                    <a role="menuitem" aria-label="Download results">
                         <svg viewBox="0 0 24 24" class="icon mdi">
                             <path d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
                         </svg>
