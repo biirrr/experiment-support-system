b89df657238b8ae961905b2a265683b7c036bcaf
Mark Hall <mark.hall@work.room3b.eu>
2021-01-18 15:38:48 +0000

    Added favicon

--- src/ess/views/default.py
+++ src/ess/views/default.py
@@ -1,6 +1,17 @@
+from importlib.resources import open_binary
+from pyramid.response import Response
 from pyramid.view import view_config
 
 
 @view_config(route_name='root', renderer='ess:templates/root.jinja2')
 def root(request):
     return {}
+
+
+@view_config(route_name="favicon")
+def favicon_view(request):
+    favicon = open_binary('ess.static', 'favicon.ico')
+    return Response(body=favicon.read(),
+                    headerlist=[
+                        ('Content-type', 'image/x-icon')
+                    ])
--- tests/util.ts
+++ tests/util.ts
@@ -1,10 +1,10 @@
-import { request as nodeRequest } from 'http';
+import { request as nodeRequest, IncomingMessage } from 'http';
 
-export async function request(url) {
+export async function request(url: string): Promise<IncomingMessage> {
     return new Promise((resolve, reject) => {
         const req = nodeRequest(url, (res) => {
             if (res.statusCode === 200) {
-                resolve();
+                resolve(res);
             } else {
                 reject();
             }
