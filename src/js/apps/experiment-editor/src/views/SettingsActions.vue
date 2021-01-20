2318de9e873ec48c8473a33fb2371587e185b6e3
Mark Hall <mark.hall@work.room3b.eu>
2021-01-20 13:00:52 +0000

    Implemented deleting an experiment

---
+++ src/js/apps/experiment-editor/src/views/SettingsActions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="settings-actions">
+        <form :action="$store.state.config.experiment.deleteUrl" method="post" @submit="confirmDeleteExperiment">
+            <input type="hidden" name="csrf_token" :value="$store.state.config.dataStore.csrfToken"/>
+            <button class="button alert">Delete this Experiment</button>
+            <p>This will permanently delete this experiment and all data that is associated with it. This step <strong>cannot</strong> be undone.</p>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {
+    }
+})
+export default class SettingsActions extends Vue {
+    public confirmDeleteExperiment(ev: Event): void {
+        if (!confirm('Please confirm that you wish to delete this experiment and all data that is associated with it.')) {
+            ev.preventDefault();
+        }
+    }
+}
+</script>
